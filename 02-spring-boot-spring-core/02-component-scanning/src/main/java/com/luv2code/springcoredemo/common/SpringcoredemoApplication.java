7d328a568c55915d4305a10c93e7c83db5befd01
Daruan <daruan.rst@gmail.com>
2024-09-25 20:19:08 -0300

    40. Component Scanning - Coding - Part 2

--- 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luv2code/springcoredemo/common/SpringcoredemoApplication.java
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luv2code/springcoredemo/common/SpringcoredemoApplication.java
@@ -1,8 +1,12 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication(
+//		scanBasePackages = {"com.luv2code.springcoredemo",
+//							"com.luv2code.util"}
+//)
 @SpringBootApplication
 public class SpringcoredemoApplication {
 
