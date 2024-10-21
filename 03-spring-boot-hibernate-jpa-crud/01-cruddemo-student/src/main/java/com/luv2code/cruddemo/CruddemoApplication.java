365da80d3279fb0ce87629eff474c5f025f85c40
Daruan <daruan.rst@gmail.com>
2024-10-21 20:44:33 -0300

    82. Updating Objects with JPA - Coding

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -27,8 +27,30 @@ public class CruddemoApplication {
 
 //			queryForStudents(studentDAO);
 
-            queryForStudentsByLastName(studentDAO);
+//            queryForStudentsByLastName(studentDAO);
+            
+            updateStudent(studentDAO);
         };
+    }
+
+    private void updateStudent(StudentDAO studentDAO) {
+
+        // retrieve student based on the id: primary key
+        int studentId = 1;
+        System.out.println("Getting student with id: " + studentId);
+        Student myStudent = studentDAO.findById(1);
+
+        // change first name to "Scooby"
+        System.out.println("Updating student ...");
+        myStudent.setFirstName("Scooby");
+
+        // update the student
+        studentDAO.update(myStudent);
+
+        // display the updated student
+        System.out.println("Updated Student: " + myStudent);
+
+
     }
 
     private void queryForStudentsByLastName(StudentDAO studentDAO) {
