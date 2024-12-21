c62f595378f484e52babf7771c62e0b5a5fa6c62
Daruan <daruan.rst@gmail.com>
2024-12-21 11:49:31 -0300

    137. Spring Boot REST: Spring Data REST Configs and Sorting - Coding

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee - with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee - with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it no need to write any code
