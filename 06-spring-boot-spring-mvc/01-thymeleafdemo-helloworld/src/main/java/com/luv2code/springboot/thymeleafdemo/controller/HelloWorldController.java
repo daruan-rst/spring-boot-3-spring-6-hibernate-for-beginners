13711418cb22670e09721b65e44fce6fffd4cdb6
Daruan <daruan.rst@gmail.com>
2025-01-17 23:41:33 -0300

    167. Spring Boot - Hello World Form and Model - Coding - Part 1

---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    // need a controller method to show the initial HTML form
+
+    @RequestMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+}
