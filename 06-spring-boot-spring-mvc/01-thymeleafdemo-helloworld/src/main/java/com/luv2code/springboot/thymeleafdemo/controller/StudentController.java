e2e74b9b733d5e2ec7efd7af9b13b5333a40f41c
Daruan <daruan.rst@gmail.com>
2025-02-05 23:40:19 -0300

    179. Spring Boot - Spring MVC Form Data Binding - Text Fields - Coding - Part 2

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -4,12 +4,14 @@ import com.luv2code.springboot.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
 
 
-    @GetMapping("showStudentForm")
+    @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
 
         // create a student object
@@ -21,5 +23,13 @@ public class StudentController {
 
         return "student-form";
 
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+
+        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+
+        return "student-confirmation";
     }
 }
