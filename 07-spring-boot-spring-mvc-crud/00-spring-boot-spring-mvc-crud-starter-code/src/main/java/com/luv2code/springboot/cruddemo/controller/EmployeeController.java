252e9ba33e6974b0a2e5dbe662aa776d66828349
Daruan <daruan.rst@gmail.com>
2025-04-06 14:46:15 -0300

    223. CRUD Database Project - Get Employees - Coding - Part 1

---
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,37 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService theEmployeeService){
+        this.employeeService = theEmployeeService;
+    }
+
+    // add mapping for "/list"
+
+    @GetMapping("/list")
+    public String listEmployee(Model theModel){
+
+        // get the employees from the db
+        List<Employee> theEmployees = employeeService.findAll();
+
+        // add to the spring model
+        theModel.addAttribute("employees", theEmployees);
+
+        return "list-employees";
+    }
+
+
+}
