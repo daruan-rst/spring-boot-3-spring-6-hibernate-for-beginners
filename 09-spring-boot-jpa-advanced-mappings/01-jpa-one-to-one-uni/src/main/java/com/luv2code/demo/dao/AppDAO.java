98cf7b6d0b471fbc9f6072f804f56b499f07e00e
Daruan <daruan.rst@gmail.com>
2026-02-09 16:58:26 -0300

    294. @OneToOne Mapping - Coding - Create the DAO

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/demo/dao/AppDAO.java
@@ -0,0 +1,8 @@
+package com.luv2code.demo.dao;
+
+import com.luv2code.demo.entity.Instructor;
+
+public interface AppDAO  {
+
+    void save (Instructor theInstructor);
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/demo/dao/AppDAOImpl.java
@@ -0,0 +1,25 @@
+package com.luv2code.demo.dao;
+
+import com.luv2code.demo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+public class AppDAOImpl implements AppDAO {
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using contructor injection
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+}
